b856b2087fc1fd0aee1dc05fd34cf66acb48eacb
shreestipokhrel <pokhrel.shreesti@gmail.com>
2024-06-19 08:34:17 +0545

    SP: report utils and controller to fetch data from device

---
+++ controllers/ReportController.js
@@ -0,0 +1,31 @@
+//report controller
+const fetchXlsx = require('../utils/fetchXlsx');
+
+exports.getReport = async (req, res) => {
+  try {
+    const data = await fetchXlsx();
+    res.render('report', { data });
+  } catch (error) {
+    res.status(500).send('Error generating report');
+  }
+};
+
+exports.getReplay = (req, res) => {
+  // Process and return replay data
+  res.send('Replay data');
+};
+
+exports.getTripSummary = (req, res) => {
+  // Process and return trip summary data
+  res.send('Trip summary data');
+};
+
+exports.getLogs = (req, res) => {
+  // Process and return logs data
+  res.send('Logs data');
+};
+
+exports.getStatistics = (req, res) => {
+  // Process and return statistics data
+  res.send('Statistics data');
+};
